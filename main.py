bad7a6249f67610df7198f4b981cc2a181715e9c
sheba <snoffysheba91@gmail.com>
2022-10-28 20:47:15 -0700

    Adding changes to support postgreSQL

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///users.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
